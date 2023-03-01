794311b4bfc7106a2f8f5fc9813b8c639a795fe2
Andrew Northall <andrew@northall.me.uk>
2023-03-01 00:07:17 +0000

    Move UserCreationForm and UserChangeForm to forms.py

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,59 +1,8 @@
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.core.exceptions import ValidationError
-
 from .models import CavingUser
-
-
-class UserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput,
-        help_text="Your password can't be too similar to your other personal information, must contain at least 8 characters, cannot be entirely numeric and must not be a commonly used password.",
-    )
-    password2 = forms.CharField(
-        label="Confirm password",
-        widget=forms.PasswordInput,
-        help_text="Enter the same password as before, for verification.",
-    )
-
-    class Meta:
-        model = CavingUser
-        fields = ("first_name", "last_name", "email", "username")
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-
-class UserChangeForm(forms.ModelForm):
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = CavingUser
-        fields = (
-            "email",
-            "password",
-            "first_name",
-            "last_name",
-            "location",
-            "bio",
-            "is_active",
-        )
+from .forms import UserCreationForm, UserChangeForm
 
 
 class CavingUserAdmin(BaseUserAdmin):
--- app/users/forms.py
+++ app/users/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+from .models import CavingUser
 
 
 class LoginForm(forms.Form):
@@ -7,3 +10,52 @@ class LoginForm(forms.Form):
     password = forms.CharField(
         label="Password", required=True, widget=forms.PasswordInput()
     )
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput,
+        help_text="Your password can't be too similar to your other personal information, must contain at least 8 characters, cannot be entirely numeric and must not be a commonly used password.",
+    )
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput,
+        help_text="Enter the same password as before, for verification.",
+    )
+
+    class Meta:
+        model = CavingUser
+        fields = ("first_name", "last_name", "email", "username")
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = CavingUser
+        fields = (
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "location",
+            "bio",
+            "is_active",
+        )
