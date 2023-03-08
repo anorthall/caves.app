67495c87da02a0fb250e544021632b195094eeef
Andrew Northall <andrew@northall.me.uk>
2023-03-08 17:07:09 +0000

    Separate out index pages for guests/users

--- app/logger/views.py
+++ app/logger/views.py
@@ -3,4 +3,21 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "index.html")
+    """
+    Index page for the entire website
+
+    Unregistered users will be shown a static welcome page
+
+    Registered users will be shown an interface to add/manage
+    recent caving trips.
+    """
+
+    # Unregistered/unauthenticated users
+    if not request.user.is_authenticated:
+        return render(request, "index_unregistered.html")
+
+    # Authenticated users
+    context = {
+        "user": request.user,
+    }
+    return render(request, "index.html", context)
