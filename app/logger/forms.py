dc6922f8255fa7c7b93a499ea1717acb46827bbc
Andrew Northall <andrew@northall.me.uk>
2023-03-15 17:55:19 +0000

    Improve start/end validation

--- app/logger/forms.py
+++ app/logger/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.utils import timezone
 from django.core.exceptions import ValidationError
+from datetime import timedelta
 from .models import Trip
 
 
@@ -9,18 +11,47 @@ class TripForm(forms.ModelForm):
     class Meta:
         model = Trip
         exclude = ["user"]
-
         widgets = {
             "start": forms.DateTimeInput(attrs={"type": "datetime-local"}),
             "end": forms.DateTimeInput(attrs={"type": "datetime-local"}),
         }
 
+    def clean_start(self):
+        """Validate the trip start date/time"""
+        # Trips must not start more than a week in the future.
+        one_week_from_now = timezone.now() + timedelta(days=7)
+        if self.cleaned_data.get("start") > one_week_from_now:
+            raise ValidationError(
+                "Trips must not start more than one week in the future."
+            )
+        return self.cleaned_data["start"]
+
     def clean_end(self):
-        start = self.cleaned_data["start"]
-        end = self.cleaned_data["end"]
-        if end:
+        """Validate the trip end date/time"""
+        # Trips must not end more than 31 days in the future.
+        one_month_from_now = timezone.now() + timedelta(days=31)
+        if self.cleaned_data.get("end") > one_month_from_now:
+            raise ValidationError("Trips must not end more than 31 days in the future.")
+        return self.cleaned_data["end"]
+
+    def clean(self):
+        """Validate relations between the start/end datetimes"""
+        super().clean()
+
+        start = self.cleaned_data.get("start")
+        end = self.cleaned_data.get("end")
+
+        if end and start:
+            # Ensure the start time is before the end time
             if start > end:
-                raise ValidationError(
-                    "The trip start time must be before the trip end time."
+                self.add_error(
+                    "start", "The trip start time must be before the trip end time."
                 )
-        return end
+
+            # Check the trip is not unrealistically long
+            length = end - start
+            if length > timedelta(days=60):
+                self.add_error(
+                    "end",
+                    "The trip is unrealistically long in duration (over 60 days).",
+                )
