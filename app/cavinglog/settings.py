16ba18d301c750fb5fed13f28a8dc80dd89bfe05
Andrew Northall <andrew@northall.me.uk>
2023-03-30 14:53:44 +0100

    Humanise user and trip count

--- app/cavinglog/settings.py
+++ app/cavinglog/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     "widget_tweaks",
     "django_countries",
     "tinymce",
