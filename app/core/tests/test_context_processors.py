ca10786b2815be13dfe452831b50b6331d4b8958
Andrew Northall <andrew@northall.me.uk>
2023-05-27 10:57:33 +0100

    Test Google Analytics context processor

---
+++ app/core/tests/test_context_processors.py
@@ -0,0 +1,18 @@
+from django.test import TestCase, override_settings, tag
+
+from ..context_processors import google_analytics
+
+
+@tag("fast, middleware", "unit")
+class TestContextProcessors(TestCase):
+    @override_settings(GOOGLE_ANALYTICS_ID="test-google-analytics-id")
+    def test_google_analytics_context_processor_returns_id(self):
+        """Test that the Google Analytics context processor returns the correct value"""
+        result = google_analytics(None)
+        self.assertEqual(result, {"google_analytics_id": "test-google-analytics-id"})
+
+    @override_settings(GOOGLE_ANALYTICS_ID=None)
+    def test_google_analytics_context_processor_returns_empty(self):
+        """Test that the Google Analytics context processor when setting unset"""
+        result = google_analytics(None)
+        self.assertEqual(result, {})
