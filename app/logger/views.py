59bd89e5e766a457b6c53389927ea06948bb5847
Andrew Northall <andrew@northall.me.uk>
2023-05-27 02:04:01 +0100

    Move CSV export code out of the view

--- app/logger/views.py
+++ app/logger/views.py
@@ -1,7 +1,5 @@
-import csv
-
+import logger.csv
 from core.models import News
-from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.decorators import login_required
@@ -9,11 +7,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.exceptions import PermissionDenied
 from django.db.models import Count, Exists, OuterRef
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.utils import timezone
-from django.utils.timezone import localtime as lt
 from django.views.generic import (
     CreateView,
     ListView,
@@ -29,7 +25,6 @@ from . import feed
 from .forms import AllUserNotificationForm, TripForm, TripReportForm
 from .mixins import TripContextMixin, ViewableObjectDetailView
 from .models import Trip, TripReport
-from .templatetags.logger_tags import distformat
 
 User = get_user_model()
 
@@ -477,104 +472,17 @@ class HTMXTripLike(LoginRequiredMixin, TemplateView):
         return trip
 
 
-@login_required
-def export(request):
-    """Export a user's trips to CSV file"""
-    if not request.POST:  # Display information page
+class CSVExport(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
         return render(request, "export.html")
 
-    # Generate HTTP response and the CSV file
-    qs = Trip.objects.filter(user=request.user).order_by("start", "pk")
-    if not qs:
-        messages.error(request, "You do not have any trips to export.")
-        return redirect("log:export")
-
-    timestamp = timezone.now().strftime("%Y-%m-%d-%H%M")
-    filename = f"{request.user.username}-trips-{timestamp}.csv"
-    response = HttpResponse(
-        content_type="text/csv",
-        headers={"Content-Disposition": f'attachment; filename="{filename}"'},
-    )
-
-    writer = csv.writer(response)
-
-    # Headers
-    tz = timezone.get_current_timezone()
-    writer.writerow(
-        [
-            "Number",
-            "Cave name",
-            "Cave region",
-            "Cave country",
-            "Cave URL",
-            f"Trip start ({tz})",
-            f"Trip end ({tz})",
-            "Duration",
-            "Trip type",
-            "Cavers",
-            "Clubs",
-            "Expedition",
-            "Horizontal distance",
-            "Vertical distance down",
-            "Vertical distance up",
-            "Surveyed distance",
-            "Resurveyed distance",
-            "Aid climbing distance",
-            "Notes",
-            "URL",
-            "Trip report",
-            f"Added on ({tz})",
-            f"Last updated ({tz})",
-        ]
-    )
-
-    # Content
-    units = request.user.units  # Distance units
-    tf = "%Y-%m-%d %H:%M"  # Time format to use
-    x = 1
-    for t in qs:
-        row = [  # Break row into two to process end time
-            x,
-            t.cave_name,
-            t.cave_region,
-            t.cave_country,
-            t.cave_url,
-            lt(t.start).strftime(tf),
-        ]
-
-        # End time may not exist, so check first
+    def post(self, request, *args, **kwargs):
         try:
-            row = row + [lt(t.end).strftime(tf)]
-        except AttributeError:
-            row = row + [t.end]
-
-        trip_report = ""
-        if t.has_report:
-            trip_report = f"{settings.SITE_ROOT}{t.report.get_absolute_url()}"
-
-        row = row + [  # Second half of row
-            t.duration_str,
-            t.type,
-            t.cavers,
-            t.clubs,
-            t.expedition,
-            distformat(t.horizontal_dist, units, simplify=False),
-            distformat(t.vert_dist_down, units, simplify=False),
-            distformat(t.vert_dist_up, units, simplify=False),
-            distformat(t.surveyed_dist, units, simplify=False),
-            distformat(t.resurveyed_dist, units, simplify=False),
-            distformat(t.aid_dist, units, simplify=False),
-            t.notes,
-            f"{settings.SITE_ROOT}{t.get_absolute_url()}",
-            trip_report,
-            lt(t.added).strftime(tf),
-            lt(t.updated).strftime(tf),
-        ]
-
-        writer.writerow(row)  # Finally write the complete row
-        x += 1
-
-    return response  # Return the CSV file as a HttpResponse
+            http_response = logger.csv.generate_csv_export(request.user)
+        except Trip.DoesNotExist:
+            messages.error(request, "You do not have any trips to export.")
+            return redirect("log:export")
+        return http_response
 
 
 @login_required
