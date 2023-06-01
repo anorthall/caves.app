d4e295772e94cb09adfd4918db6bcefebab6edda
Andrew Northall <andrew@northall.me.uk>
2023-06-01 05:31:50 +0100

    Update error pages.

--- app/core/views.py
+++ app/core/views.py
@@ -9,3 +9,23 @@ User = get_user_model()
 class Help(TemplateView):
     template_name = "help.html"
     extra_context = {"faqs": FAQ.objects.all().order_by("updated")}
+
+
+class HTTP400(TemplateView):
+    template_name = "400.html"
+
+
+class HTTP403(TemplateView):
+    template_name = "403.html"
+
+
+class HTTP403CSRF(TemplateView):
+    template_name = "403_csrf.html"
+
+
+class HTTP404(TemplateView):
+    template_name = "404.html"
+
+
+class HTTP500(TemplateView):
+    template_name = "500.html"
