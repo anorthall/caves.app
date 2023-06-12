c1ff50e700d292e456a11564ebfc3a43557a2a4d
Andrew Northall <andrew@northall.me.uk>
2023-06-12 18:07:10 +0100

    Add Trip Photo statistics

--- app/staff/views.py
+++ app/staff/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.views.generic import RedirectView, TemplateView
-from logger.models import Trip, TripReport
+from logger.models import Trip, TripPhoto, TripReport
 
 from .mixins import StaffRequiredMixin
 from .statistics import get_time_statistics
@@ -17,12 +17,16 @@ class Dashboard(StaffRequiredMixin, TemplateView):
         trips = Trip.objects.all()
         trip_reports = TripReport.objects.all()
         users = User.objects.all()
+        photos = TripPhoto.objects.all()
+        photos_valid = photos.filter(is_valid=True)
 
         statistics = [
             get_time_statistics(trips),
             get_time_statistics(trips, metric="Updated", lookup="updated__gte"),
             get_time_statistics(trip_reports),
             get_time_statistics(trip_reports, metric="Updated", lookup="updated__gte"),
+            get_time_statistics(photos, metric="New", lookup="added__gte"),
+            get_time_statistics(photos_valid, metric="Valid", lookup="added__gte"),
             get_time_statistics(users, metric="New", lookup="date_joined__gte"),
             get_time_statistics(users, metric="Active", lookup="last_seen__gte"),
         ]
