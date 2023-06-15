98ded9f424955678b8642298492341ab47bb671c
Andrew Northall <andrew@northall.me.uk>
2023-06-15 01:06:17 +0100

    Add management command to prune inactive users (#11)

---
+++ app/core/management/commands/prune_inactive_users.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from users.models import CavingUser as User
+
+
+class Command(BaseCommand):
+    help = "Delete all users older than 24 hours where is_active=False"
+
+    def handle(self, *args, **options):
+        td = timezone.now() - timezone.timedelta(hours=24)
+
+        users_to_delete = User.objects.filter(
+            is_active=False,
+            date_joined__lte=td,
+        )
+
+        count = users_to_delete.count()
+        users_to_delete.delete()
+
+        self.stdout.write(self.style.SUCCESS(f"Deleted {count} unverified users."))
