f5dd1242cd847c656ce91c68f8021aeeaeb863a9
Andrew <andrew@northall.me.uk>
2023-06-21 10:36:05 +0200

    Add full path to binary

--- bin/aws-backup.sh
+++ bin/aws-backup.sh
@@ -6,15 +6,20 @@
 
 
 # Configuration
+
+# For cron - otherwise it won't find configuration or the binary
+export HOME=/home/caves
+AWS=/usr/local/bin/aws
+
 APP_ROOT=/home/caves/caves.app
 BUCKET=s3://caves.app-backup
 LOGFILE=$APP_ROOT/data/production/logs/aws/s3-sync.log
 
 
-aws s3 sync $APP_ROOT/data/production/backups/db $BUCKET/db \
+$AWS s3 sync $APP_ROOT/data/production/backups/db $BUCKET/db \
     --exclude "**/postgres-latest.sql.gz" >> $LOGFILE
-aws s3 sync $APP_ROOT/data/production/backups/crontab $BUCKET/crontab >> $LOGFILE
-aws s3 sync $APP_ROOT/data/production/logs $BUCKET/logs >> $LOGFILE
-aws s3 sync $APP_ROOT/bin $BUCKET/bin >> $LOGFILE
-aws s3 sync $APP_ROOT/config $BUCKET/config/app >> $LOGFILE
-aws s3 sync /etc/nginx/sites-available $BUCKET/config/nginx/sites-available >> $LOGFILE
+$AWS s3 sync $APP_ROOT/data/production/backups/crontab $BUCKET/crontab >> $LOGFILE
+$AWS s3 sync $APP_ROOT/data/production/logs $BUCKET/logs >> $LOGFILE
+$AWS s3 sync $APP_ROOT/bin $BUCKET/bin >> $LOGFILE
+$AWS s3 sync $APP_ROOT/config $BUCKET/config/app >> $LOGFILE
+$AWS s3 sync /etc/nginx/sites-available $BUCKET/config/nginx/sites-available >> $LOGFILE
