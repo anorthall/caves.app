de2bd7c00344d05e0762f78ac0e5bd54918abfe4
Andrew Northall <andrew@northall.me.uk>
2023-07-10 14:21:51 +0200

    Add staging Django settings file

---
+++ config/django/settings/staging.py
@@ -0,0 +1,17 @@
+from .production import *
+
+# Turn DEBUG on in staging
+DEBUG = True
+
+# Turn staticfiles off in staging to serve from CDN
+INSTALLED_APPS.remove("django.contrib.staticfiles")
+
+# Django debug toolbar
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
+# Always show the toolbar in staging as staging should never be exposed
+# to the internet anyway
+DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': lambda request: True,
+}
