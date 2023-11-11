0f3bb9cf38dc9bf0d6b78a960190ee29e933b238
Andrew Northall <andrew@northall.me.uk>
2023-11-11 04:17:20 +0000

    Add Docker IP to INTERNAL_IPS

--- config/django/settings/development.py
+++ config/django/settings/development.py
@@ -19,8 +19,8 @@ hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + [
     "127.0.0.1",
     "10.0.2.2",
+    "192.168.65.1",
 ]
-
 
 # Storages
 STORAGES["default"] = {
