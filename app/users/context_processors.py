de07948a0b6507976c3c82bd94ec559e181f872c
Andrew Northall <andrew@northall.me.uk>
2023-12-16 21:24:38 +0000

    Optimise notification queries

--- app/users/context_processors.py
+++ app/users/context_processors.py
@@ -10,7 +10,19 @@ def notifications(request):
         "list": [],
     }
 
-    n_list = Notification.objects.filter(user=request.user).order_by("-updated")[:9]
+    n_list = (
+        Notification.objects.filter(user=request.user)
+        .select_related(
+            "user",
+            "trip",
+        )
+        .prefetch_related(
+            "user__friends",
+            "trip__user",
+        )
+        .order_by("-updated")[:9]
+    )
+
     if bool(n_list):
         for notification in n_list:
             if not notification.read:
