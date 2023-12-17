655cd4205db91e98d7876c20c7fd9dc1fd76f093
Andrew Northall <andrew@northall.me.uk>
2023-12-17 21:38:38 +0000

    Check if a user is authenticated before checking if they are a moderator

--- app/staff/mixins.py
+++ app/staff/mixins.py
@@ -1,6 +1,8 @@
+from core.utils import get_user
 from django.contrib.auth.mixins import UserPassesTestMixin
 
 
 class ModeratorRequiredMixin(UserPassesTestMixin):
     def test_func(self):
-        return self.request.user.is_moderator
+        user = get_user(self.request)
+        return user.is_authenticated and user.is_moderator
